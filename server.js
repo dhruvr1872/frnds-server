757fe5819a71dd1a701233ceca938982fdddcd61
Aaditya Dhruv <aadityadhruv11@gmail.com>
2021-12-12 17:14:43 -0600

    try catch

--- server.js
+++ server.js
@@ -51,117 +51,188 @@ app.get("/", (req, res) => {
 
 //create user
 app.post("/createuser", (req, res) => {
+  try {
   createUser(req.body);
   res.json({ message: "OK", data: req.body });
+  } catch (e) {
+    res.json({ message: "error", data: e });
+  }
+
 });
 
 //get user classes
 app.get("/classes", async (req, res) => {
+  try {
   await getClasses(req.query.id).then((classes) => {
     res.json({ message: "OK", data: classes });
   });
+}
+
+
+  catch (e) {
+    res.json({ message: "error", data: e });
+  }
 });
 
 //get friends in class
 app.get("/friends_classes", async (req, res) => {
+  try {
   await friendsInClass(req.query.id, req.query.classId).then((friends) => {
     res.json({ message: "OK", data: friends });
   });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 //get common classes
 app.get("/common_classes", async (req, res) => {
+  try {
   await commonClasses(req.query.id, req.query.friendId).then((classes) => {
     res.json({ message: "OK", data: classes });
   });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 //add class
 app.post("/add_class", async (req, res) => {
+  try {
   await addClass(req.query.id, req.query.classId).then((classes) => {
     res.json({ message: "OK", data: classes });
   });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 //remove class
 app.post("/remove_class", async (req, res) => {
+  try {
   await removeClass(req.query.id, req.query.classId).then((classes) => {
     res.json({ message: "OK", data: classes });
   });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 //update user
 app.put("/update_user", async (req, res) => {
+  try {
   await updateUsername(req.body.id, req.body.username);
   await updatePhone(req.body.id, req.body.phone);
   await updateYear(req.body.id, req.body.year);
   res.json({ message: "OK", data: req.body });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 //send friend request
 app.post("/send_friend_request", async (req, res) => {
+  try {
   await sendRequest(req.query.id, req.query.friendId);
   res.json({ message: "OK", data: req.query });
+  } catch (e) {
+    res.json({ message: "error", data: e });
+  }
+
 });
 
 //accept friend request
 app.post("/accept_friend_request", async (req, res) => {
+  try {
   await acceptRequest(req.query.id, req.query.friendId);
   res.json({ message: "OK", data: req.query });
+  } catch (e) {
+    res.json({ message: "error", data: e });
+  }
 });
 
 //decline friend request
 app.post("/decline_friend_request", async (req, res) => {
+  try {
   await declineRequest(req.query.id, req.query.friendId);
   res.json({ message: "OK", data: req.query });
+  } catch (e) {
+    res.json({ message: "error", data: e });
+  }
 });
 
 //remove friend
 app.post("/remove_friend", async (req, res) => {
+  try {
   await removeFriend(req.query.id, req.query.friendId);
   res.json({ message: "OK", data: req.query });
+  } catch (e) {
+    res.json({ message: "error", data: e });
+  }
+
 });
 
 //check if has sent request
 app.get("/has_sent_request", async (req, res) => {
+  try {
   await hasSentRequest(req.query.id, req.query.friendId).then((data) => {
     res.json({ message: "OK", data: data });
   });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 
 app.get("/user", async (req, res) => {
+  try {
   var user = await getUserById(req.query.id);
   res.json({ message: "OK", data: user });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
   });
 
 
 
   //get all users
   app.get("/users", async (req, res) => {
+    try {
     var users = await getAllUsers();
     res.json({ message: "OK", data: users });
+    } catch (e) {
+      res.json({ message: "error", data: e });
+    }
     });
 
 
 
 //check if recieved request
 app.get("/has_received_request", async (req, res) => {
+  try {
   await hasRecievedRequest(req.query.id, req.query.friendId).then((data) => {
     res.json({ message: "OK", data: data });
   });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 //check if two users are friends
 app.get("/check_friends", async (req, res) => {
+  try {
   await areFriends(req.query.id, req.query.friendId).then((friends) => {
     res.json({ message: "OK", data: friends });
   });
+} catch (e) {
+  res.json({ message: "error", data: e });
+}
 });
 
 //API Call
 
 app.get("/course", (req, res) => {
+  try {
   const { year, semester, subject, code, crn } = req.query;
 
   if (crn) {
@@ -193,6 +264,10 @@ app.get("/course", (req, res) => {
           res.send({ message: "OK", data: response.data });
         });
     }
+  } catch (e) {
+    res.send({ message: "error", data: e });
+  }
+
 });
 
 app.listen(PORT, function () {
